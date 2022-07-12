b901bac0b1ae38c098392321b7b472807f7c8a3c
Lucas Antunes de Freitas <lucas.afreitas@meta.com.br>
2022-07-12 18:13:56 -0300

    Creating component Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,21 +2,52 @@ import styled from "styled-components";
 
 interface InputProps {
     placeholder: string;
-    height: string;
-    width: string;
+    height: number;
+    width: number;
     fontSize: string;
+    borderColor: string;
+    border: number;
+    borderRadius: number;
+    outlineColorFocus: string;
+    backgroundColor: string;
+    iconUrl: string;
 }
 
 const Input = (props: InputProps) => {
 
     const Input = styled.input`
-        height: ${props.height};
-        width: ${props.width};
-        font-size: ${props.fontSize};        
+        width: ${props.width}px;
+        height: ${props.height}px;        
+        font-size: ${props.fontSize}; 
+        border: ${props.border} solid ${props.borderColor};        
+        border-radius: ${props.borderRadius};
+        margin-left: 60px;
+        :focus {
+            outline-color: ${props.outlineColorFocus};            
+        }
+    `;
+
+    const FormControl = styled.form`
+        display: flex;
+        flex-direction: row;        
+        width: ${props.width + 60}px;
+        height: ${props.height}px;        
+        background: ${props.backgroundColor};
+        border: ${props.border} solid ${props.borderColor};        
+        border-radius: ${props.borderRadius};        
+    `;
+
+    const Image = styled.img`
+        display: flex;
+        align-items: center;
+        justify-content: center;
     `;
 
     return (
-        <Input placeholder={props.placeholder} height={props.height} width={props.width} />
+        <FormControl>
+            <Image src={props.iconUrl} alt="" height={20} width={20}/>
+            <Input placeholder={props.placeholder}></Input>
+        </FormControl>
     );
 }
 
--- stories/input.stories.tsx
+++ stories/input.stories.tsx
@@ -15,8 +15,13 @@ const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
 export const InputNewsletter = Template.bind({});
 InputNewsletter.args = {
     placeholder: 'Insira seu e-mail',
-    height: '75px',
-    width: '390px',
-    fontSize: '16pt'
+    height: 75,
+    width: 390,
+    fontSize: '16pt',
+    backgroundColor: '#ffffff',
+    borderRadius: 4,
+    border: 0,
+    outlineColorFocus: 'transparent',
+    iconUrl: 'https://github.com/lukzfreitas/7-days-of-code-react/blob/main/public/logo.png?raw=true'
 }
 
