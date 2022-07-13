ef84dfd6e425eeabe458e643388959d178a0c2e6
Lucas Antunes de Freitas <lucas.afreitas@meta.com.br>
2022-07-13 14:36:30 -0300

    Improving component button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -25,16 +25,15 @@ const Button = ({
   const ButtonStyle = styled.div`
     font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
     font-weight: 700;
-    border: 0;
-    border-radius: 3em;
+    border: 0;    
     cursor: pointer;
     display: inline-block;
     line-height: 1;
     color: ${primary ? 'white' : '#333'};
-    background-color: ${primary ? '#1ea7fd' : 'transparent'};
+    background-color: ${primary ? '#FFCB47' : 'transparent'};
     box-shadow: ${!primary ? 'rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset' : 'none'};
     font-size: ${size == 'small' ? '12px' : size == 'medium' ? '14px' : '16px'};
-    padding: ${size == 'small' ? '10px 16px' : size == 'medium' ? '11px 20px' : '12px 24px'}
+    padding: ${size == 'small' ? '10px 16px' : size == 'medium' ? '11px 20px' : '30px 80px'};
   `;
 
   return (    
