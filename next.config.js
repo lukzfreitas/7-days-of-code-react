e7c0a16f6013caa3ce1d5f8853464168c305f253
Lucas Antunes de Freitas <lucas.afreitas@meta.com.br>
2022-07-20 16:35:04 -0300

    Adding property compile styled components equals true in file next.config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   images: {
     domains: ['github.com', 'raw.githubusercontent.com'],
   },
+  compiler: {    
+    styledComponents: true
+  }
 }
 
 module.exports = nextConfig
